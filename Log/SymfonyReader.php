c9b6d7c3ce43578f1ccf4a8497d8114ed717abf7
Henrik Westphal <westphal@secure-net-concepts.de>
2011-10-30 10:48:33 +0100

    Added license information.

--- Log/SymfonyReader.php
+++ Log/SymfonyReader.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SecotrustRouteStatisticsBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Secotrust\Bundle\RouteStatisticsBundle\Log;
 
 class SymfonyReader extends Reader
